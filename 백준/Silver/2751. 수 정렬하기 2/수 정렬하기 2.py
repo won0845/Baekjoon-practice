ad9a996c4cc9cf3c7be046ef0062d2fd90a855c8
won0845 <153366472+won0845@users.noreply.github.com>
2024-01-16 16:06:13 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 4568 ms, Memory: 78928 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,8 +1,45 @@
+import sys
+
+input = sys.stdin.readline
+
 n = int(input())
 lst = []
+buff =[None] * n
+
 for i in range(n):
     lst.append(int(input()))
 
-lst.sort()
+
+def merge_sort(a, left, right):
+    if left < right:
+        center = (left + right) // 2
+
+        merge_sort(a, left, center)
+        merge_sort(a, center + 1, right)
+
+        p = j = 0
+        i = k = left
+
+        while i <= center:
+            buff[p] = a[i]
+            p += 1
+            i += 1
+
+        while i <= right and j < p:
+            if buff[j] <= a[i]:
+                a[k] = buff[j]
+                j += 1
+            else:
+                a[k] = a[i]
+                i += 1
+            k += 1
+
+        while j < p:
+            a[k] = buff[j]
+            k += 1
+            j += 1
+    return a
+
+lst = merge_sort(lst, 0, len(lst) - 1)
 for i in lst:
     print(i)

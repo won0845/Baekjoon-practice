3a43a9fd7acbf365a48b5155e5426063e7bbb9b3
won0845 <153366472+won0845@users.noreply.github.com>
2024-01-22 17:38:40 +0900

    [Gold V] Title: 이진 검색 트리, Time: 2188 ms, Memory: 34828 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,13 +1,15 @@
 import sys
+from collections import deque
 
 sys.setrecursionlimit(10 ** 9)
 
-preorder_arr = []
+input = sys.stdin.readline
 
-# 엔터 들어올 때까지 입력
+preorder = []
+
 while True:
     try:
-        preorder_arr.append(int(sys.stdin.readline().rstrip()))
+        preorder.append(int(input()))
     except:
         break
 
@@ -16,24 +18,19 @@ def postorder(root_idx, end_idx):
     if root_idx > end_idx:
         return
 
-    global preorder_arr
+    global preorder
 
-    # 만약 root보다 큰 값 없는 경우 전부 왼쪽 서브트리로 취급
     right_start = end_idx + 1
 
     for i in range(root_idx + 1, end_idx + 1):
-        if preorder_arr[root_idx] < preorder_arr[i]:
+        if preorder[root_idx] < preorder[i]:
             right_start = i
             break
-
-    # root 다음부터 왼쪽 서브트리 탐색
     postorder(root_idx + 1, right_start - 1)
 
-    # 왼쪽 서브트리 탐색 끝나면 오른쪽 서브트리 탐색
     postorder(right_start, end_idx)
 
-    # 왼쪽, 오른쪽 서브트리 탐색 끝나면 root 출력
-    print(preorder_arr[root_idx])
+    print(preorder[root_idx])
 
 
-postorder(0, len(preorder_arr) - 1)
+postorder(0, len(preorder) - 1)

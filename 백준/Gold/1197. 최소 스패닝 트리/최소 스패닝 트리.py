18a2edc7c036d71cb04aa425493fb6ebb217ac1a
won0845 <153366472+won0845@users.noreply.github.com>
2024-01-22 13:44:29 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 4428 ms, Memory: 56384 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,32 @@
+V, E = map(int, input().split())
+tree = []
+
+for _ in range(E):
+    tree.append(list(map(int, input().split())))
+
+root = list(range(V + 1))
+
+
+def find(x):
+    if x != root[x]:
+        root[x] = find(root[x])
+    return root[x]
+
+
+def union(a, b):
+    a, b = find(a), find(b)
+    if a > b:
+        root[a] = b
+    else:
+        root[b] = a
+
+
+tree.sort(key=lambda x: x[2])
+
+ans = 0
+for a, b, c in tree:
+    if find(a) != find(b):
+        union(a, b)
+        ans += c
+
+print(ans)

d86fa0dba58673baed7a543e025c0e2422970665
won0845 <153366472+won0845@users.noreply.github.com>
2024-01-24 11:08:27 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,37 +1,32 @@
+import sys
 from collections import deque
+
+sys.setrecursionlimit(10 ** 6)
+
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-graph = []
+mapList = []
 
 for _ in range(N):
-    graph.append(list(map(int, input())))
+    mapList.append(list(map(int, input().strip())))
 
 
 def bfs(x, y):
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-
     q = deque([(x, y)])
-
     while q:
         x, y = q.popleft()
-
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-
             if nx < 0 or nx >= N or ny < 0 or ny >= M:
                 continue
-
-            if graph[nx][ny] == 0:
-                continue
-
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
+            if mapList[nx][ny] == 1:
+                mapList[nx][ny] = mapList[x][y] + 1
                 q.append((nx, ny))
-
-    return graph[N - 1][M - 1]
-
+    return mapList[N-1][M-1]
 
 print(bfs(0, 0))

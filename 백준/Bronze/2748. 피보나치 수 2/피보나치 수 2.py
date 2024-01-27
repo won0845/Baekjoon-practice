1bf612d16204daf9b9b467002701ac29d065153d
won0845 <153366472+won0845@users.noreply.github.com>
2024-01-27 17:09:46 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -4,15 +4,14 @@ sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
 n = int(input())
-memo = []
 
 
 def fibonaci(n):
     fibo = {0: 0, 1: 1, 2: 1}
-    if n == 0:
-        return 0
-    if n == 1 or n == 2:
-        return 1
+    #if n == 0:
+    #    return 0
+    #if n == 1 or n == 2:
+    #    return 1
     for i in range(2, n + 1):
         fibo[i] = fibo[i - 1] + fibo[i - 2]
     return fibo[n]

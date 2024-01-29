9919c0e8ee675dac6e70a95ba7361461647a744c
won0845 <153366472+won0845@users.noreply.github.com>
2024-01-29 10:54:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 6072 ms, Memory: 282332 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+item = []
+for _ in range(N):
+    item.append(list(map(int, input().split())))
+
+dp = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    for j in range(1, K + 1):
+        if item[i-1][0] <= j:
+            dp[i][j] = max(item[i-1][1] + dp[i - 1][j - item[i-1][0]], dp[i - 1][
+                j])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[N][K])

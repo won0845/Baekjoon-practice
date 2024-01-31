e7bdf9f6d1db63c3ab78d6aeedaa5816105ffe41
won0845 <153366472+won0845@users.noreply.github.com>
2024-01-31 19:00:43 +0900

    [Gold V] Title: LCS, Time: 560 ms, Memory: 56408 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 string_a = input().strip()
 string_b = input().strip()
 
-dp = [[0 for _ in range(len(string_b) + 1)] for _ in range(len(string_a) + 1)] #좌우
+dp = [[0 for _ in range(1001)] for _ in range(1001)]
 
 for i in range(1, len(string_a) + 1):
     for j in range(1, len(string_b) + 1):
@@ -14,4 +14,4 @@ for i in range(1, len(string_a) + 1):
         else:
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-print(dp[-1][-1])
+print(dp[len(string_a)][len(string_b)])

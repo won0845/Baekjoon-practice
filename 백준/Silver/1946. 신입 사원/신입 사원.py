3c5b14433d7c48f17089c1c5e13411f03155988a
won0845 <153366472+won0845@users.noreply.github.com>
2024-01-31 22:44:00 +0900

    [Silver I] Title: 신입 사원, Time: 2696 ms, Memory: 223656 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,22 +1,20 @@
 import sys
 
-sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
-test_num = int(input())
+T = int(input())
 
-for _ in range(test_num):
-    applicant_num = int(input())
-    grade_list = []
-    for _ in range(applicant_num):
-        brief_grade, interview_grade = map(int, input().split())
-        grade_list.append([brief_grade, interview_grade])
-    grade_list.sort(key=lambda x: x[0])
-
-    count = 1
-    first = grade_list[0][1]
-    for i in range(1, len(grade_list)):
-        if first >= grade_list[i][1]:
-            first = grade_list[i][1]
-            count += 1
-    print(count)
+for _ in range(T):
+    N = int(input())
+    lst = []
+    cont = 1
+    for _ in range(N):
+        A, B = map(int, input().split())
+        lst.append([A, B])
+    lst.sort(key=lambda x: x[0])
+    fir = lst[0][1]
+    for i in range(len(lst)):
+        if fir > lst[i][1]:
+            fir = lst[i][1]
+            cont += 1
+    print(cont)

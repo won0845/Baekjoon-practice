fa1ebd3eeb5dc94d056c2aa7508777be398fd668
won0845 <153366472+won0845@users.noreply.github.com>
2024-02-28 10:32:42 +0900

    [Silver V] Title: 숫자 카드, Time: 588 ms, Memory: 125632 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -2,22 +2,18 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-nlist = sorted(list(map(int, input().split())))
+N = int(input())
+cards = set(map(int, input().split()))
 
-m = int(input())
-mlist = list(map(int, input().split()))
+M = int(input())
+nums = list(map(int, input().split()))
 
-for i in mlist:
-    low, high = 0, n - 1
-    check = False
-    while low <= high:
-        mid = (low + high) // 2
-        if nlist[mid] < i:
-            low = mid + 1
-        elif nlist[mid] > i:
-            high = mid - 1
-        elif nlist[mid] == i:
-            check = True
-            break
-    print(1 if check else 0, end=" ")
+ans = []
+
+for num in nums:
+    if num in cards:
+        ans.append(1)
+    else:
+        ans.append(0)
+
+print(*ans)

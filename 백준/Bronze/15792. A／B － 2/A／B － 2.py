3568d9e980b3f503dfdb6ce0b5b0bbcb78373a2e
won0845 <153366472+won0845@users.noreply.github.com>
2024-03-03 23:53:24 +0900

    [Bronze I] Title: A/B - 2, Time: 52 ms, Memory: 31120 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.py
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.py
@@ -1,2 +1,11 @@
 a, b = map(int, input().split())
-print(a/b)
+print(a // b, end = '') # 우선 몫을 출력
+
+if a % b: # 나머지가 있다면
+    print('.', end='') # 점(.)을 출력해주고
+    i = 0
+    
+    while a % b and i < 1000: 
+        a = a % b * 10 # 계속 10씩 곱해주면서 몫을 뒤에 붙여줌 
+        i += 1
+        print(a // b, end = '')

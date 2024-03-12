f7dff65e83c4cabfef329f250e84504faa978e8b
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2024-03-12 10:54:07 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+s = input().strip()
+a = {}
+sum = 0
+alpha = "abcdefghijklmnopqrstuvwxyz"
+
+
+for k in range(0, len(alpha)):
+    a[alpha[k]] = k + 1
+
+for i in range(len(s)):
+    sum += a[s[i]]*(31**i)
+print(sum)

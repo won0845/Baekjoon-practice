275ff6a19b326d361013c82da2c75ba83cc66bfe
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2024-06-14 23:47:37 +0900

    [Bronze III] Title: ACM 호텔, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+for _ in range(n):
+    H, W, N = map(int, input().split())
+    ho = N % H
+    floor = N // H + 1
+    if ho == 0:  # N이 H의 배수일 때
+        ho = H
+        floor -= 1
+    if floor < 10:
+        print(f"{ho}0{floor}")
+    else:
+        print(f"{ho}{floor}")

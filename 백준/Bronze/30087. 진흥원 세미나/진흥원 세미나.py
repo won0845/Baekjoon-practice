b895d72bc5a422f222c3d57578f51900aa15b6b9
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2024-06-25 23:49:41 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,6 @@
+n = int(input())
+dic ={"Algorithm":"204", "DataAnalysis":"207", "ArtificialIntelligence":"302", 
+      "CyberSecurity":"B101", "Network":"303", "Startup":"501", "TestStrategy":"105"}
+for _ in range(n):
+    print(dic[input()])
+    

7da88ff7e6ed1588f9ede6f8f34476838516d8cd
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2024-06-29 10:28:44 +0900

    [Bronze IV] Title: 안밖? 밖안? 계단? 역계단?, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28290. 안밖？ 밖안？ 계단？ 역계단？/안밖？ 밖안？ 계단？ 역계단？.py
@@ -0,0 +1,12 @@
+string = input()
+
+if string == "fdsajkl;" or string == "jkl;fdsa":
+    print("in-out")
+elif string == "asdf;lkj" or string == ";lkjasdf":
+    print("out-in")
+elif string == "asdfjkl;":
+    print("stairs")
+elif string == ";lkjfdsa":
+    print("reverse")
+else:
+    print("molu")

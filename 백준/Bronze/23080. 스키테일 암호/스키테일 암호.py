c7959292814e434ab3bd7c4357491eb63acc3518
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2024-09-30 01:47:01 +0900

    [Bronze III] Title: 스키테일 암호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/23080. 스키테일 암호/스키테일 암호.py
+++ 백준/Bronze/23080. 스키테일 암호/스키테일 암호.py
@@ -1,5 +1,6 @@
-n = int(input())  # 주어진 n 값 입력
-code = input()    # 암호화된 문자열 입력
+n = int(input())
+code = input()
 
 for i in range(0, len(code), n):
-    print(code[i], end="")
+    print(code[i],end="")
+

033b258e7b0361151e7adf012c6f2ee66e5db285
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2024-10-17 01:32:41 +0900

    [Bronze IV] Title: H4x0r, Time: 48 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/26768. H4x0r/H4x0r.py
+++ 백준/Bronze/26768. H4x0r/H4x0r.py
@@ -1,9 +1,6 @@
 n = input()
+lst = {'a':'4', 'e':'3', 'i':'1', 'o':'0', 's':'5'}
 
-lst = {"a":4, "e":3, "i":1, "o":0, "s":5}
-
-for i in n:
-    if i in lst:
-        print(lst[i],end="")
-    else:
-        print(i,end="")
+for i in lst:
+    n = n.replace(i,lst[i])
+print(n)

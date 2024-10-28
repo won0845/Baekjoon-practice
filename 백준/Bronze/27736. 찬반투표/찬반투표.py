880b55e1296f8f0ed9d466f62a76d895b69fbef9
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2024-10-29 00:04:58 +0900

    [Bronze III] Title: 찬반투표, Time: 32 ms, Memory: 31120 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/27736. 찬반투표/찬반투표.py
@@ -0,0 +1,16 @@
+n = int(input())
+lst = list(map(int, input().split()))
+
+jun = len(lst)
+chan = lst.count(1)  
+ban = lst.count(-1)
+gi = jun-chan-ban
+
+if jun // 2 < gi:
+    print("INVALID")
+elif chan > ban:
+    print("APPROVED")
+else:
+    print("REJECTED")
+
+

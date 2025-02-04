cc7c20d1466ee9b192903f814444f892e5127155
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2025-02-04 18:20:42 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 44 ms, Memory: 34476 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,16 +1,14 @@
-import sys
+import bisect
 
-input = sys.stdin.readline
-
-N = int(input())
+n = int(input())
 arr = list(map(int, input().split()))
+sub = []
 
-# dp 배열 초기화
-dp = [1] * N
-
-for i in range(1, N):
-    for j in range(i):
-        if arr[i] > arr[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
-
-print(max(dp))
+for i in arr:
+    pos = bisect.bisect_left(sub, i)
+    if pos == len(sub):
+        sub.append(i)
+    else:
+        sub[pos] = i
+        
+print(len(sub))

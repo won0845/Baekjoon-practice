85079d3cfb337e17fa0afa29666428e939f11911
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2025-02-24 18:11:01 +0900

    [Bronze II] Title: 수 정렬하기, Time: 268 ms, Memory: 19616 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -10,20 +10,23 @@ public class Main{
             arr[i] = Integer.parseInt(sc.nextLine());
         }
         
-        bubble_sort(arr);
+        selection_sort(arr);
         for (int i =0; i < n; i++){
             System.out.println(arr[i]);
         }
     }
-    public static void bubble_sort(int arr[]){
-        
-        for(int i=1; i < arr.length; i++){
-            for(int j = 0; j < arr.length - i; j++){
-                if(arr[j] > arr[j+1]){
-                    swap(arr, j, j+1);
-                }
-            }
-        }
+
+    
+    public static void selection_sort(int arr[]) {
+    	int min_idx = 0;
+    	for (int i = 0; i < arr.length; i++) {
+    		for(int j = i + 1; j < arr.length; j++) {
+    			if (arr[j] < arr[min_idx]) {
+    				swap(arr,min_idx, j);
+    			}
+    		}
+    		min_idx++;
+    	}
     }
     
     public static void swap(int arr[], int a, int b){

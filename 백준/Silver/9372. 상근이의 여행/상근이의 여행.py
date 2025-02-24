ec3908084c544d259999628395b625142d57f2d9
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2025-02-24 13:48:33 +0900

    [Silver IV] Title: 상근이의 여행, Time: 4004 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -3,23 +3,22 @@ n = int(input())
 for i in range(n):
     N, M = map(int, input().split())
     country = [[] for i in range(N+1)]
-    visited = [0 for i in range(N+1)]
+    visited = [False] * (N+1)
     for j in range(M):
         a, b = map(int, input().split())
         country[a].append(b)
         country[b].append(a)
 
-    stk =[]
-    stk.append(country[1][0])
+    stk =[1]
+    visited[1] =True
     cnt = 0
+    
     while stk:
         a = stk.pop()
-        if visited[a] == 0:
-            visited[a] = 1
-            cnt +=1
-            for k in country[a]:
+        for k in country[a]:        
+            if not visited[k]:
+                visited[k] = True
+                cnt +=1
                 stk.append(k)
-    print(cnt- 1)
                 
-        
-
+    print(cnt)

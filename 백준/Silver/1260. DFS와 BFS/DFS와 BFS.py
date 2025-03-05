5eab81876709135fecc15a60da683ca9a0e04674
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2025-03-05 12:58:03 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 35324 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,12 +1,11 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
 
 n, m, v = map(int, input().split())
 
-visitedDFS = [False] * (n+1)
-visitedBFS = [False] * (n+1)
+visitedDFS = [False for _ in range(n+1)]
+visitedBFS = [False for _ in range(n+1)]
 
 tree = [[] for _ in range(n+1)]
 
@@ -14,21 +13,21 @@ for _ in range(m):
     a, b = map(int, input().split())
     tree[a].append(b)
     tree[b].append(a)
+    
+for i in range(1,n+1):
+    tree[i].sort()
 
-for i in tree:
-    i.sort()
-
+    
 def dfs(v):
     visitedDFS[v] = True
     print(v, end=" ")
     for i in tree[v]:
         if not visitedDFS[i]:
             dfs(i)
-
+    
 def bfs(v):
     q = deque([v])
     visitedBFS[v] = True
-    
     while q:
         node = q.popleft()
         print(node, end=" ")
@@ -36,9 +35,9 @@ def bfs(v):
             if not visitedBFS[i]:
                 q.append(i)
                 visitedBFS[i] = True
-
 dfs(v)
 print()
 bfs(v)
-
-#정렬 필요
+    
+    
+ 

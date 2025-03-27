bd63976e60cd682890248566c3709128fe621d8c
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2025-03-27 15:11:19 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 6, Time: 556 ms, Memory: 76552 KB -BaekjoonHub

---
+++ 백준/Silver/24484. 알고리즘 수업 － 깊이 우선 탐색 6/알고리즘 수업 － 깊이 우선 탐색 6.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n, m , r = map(int, input().split())
+v = [[]for _ in range(n+1)]
+visited = [-1 for _ in range(n+1)]
+order = [0 for _ in range(n+1)]
+cnt = [1]
+
+for i in range(m):
+    a, b = map(int, input().split())
+    v[a].append(b)
+    v[b].append(a)
+
+for i in v:
+    i.sort(reverse= True)
+
+def dfs(r, depth):
+    visited[r] = depth
+    order[r] = cnt[0]
+    cnt[0] += 1
+    for i in v[r]:
+        if visited[i] == -1:
+            dfs(i, depth+1)
+
+        
+dfs(r, 0)
+
+print(sum(visited[i] * order[i] for i in range(1, n+1)))

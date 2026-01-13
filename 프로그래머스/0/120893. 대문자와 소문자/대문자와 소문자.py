4166da831a6b1e188a8f1080f289c25f954b21f1
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2026-01-13 19:47:38 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.32 ms, Memory: 9.39 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = []
+    
+    for i in my_string:
+        if i.isupper():
+            answer.append(i.lower())
+        else:
+            answer.append(i.upper())
+    
+    return "".join(answer)

5009ae35642453d96a5408fc4ad5d3495b6885d0
LEEJAEWON <153366472+won0845@users.noreply.github.com>
2026-01-13 18:55:33 +0900

    [level 0] Title: 소인수분해, Time: 0.03 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,15 +1,11 @@
 def solution(n):
     answer = []
-    i = 2
 
-    while i * i <= n:
+    for i in range(2, int(n ** 0.5) +1):
         if n % i == 0:
             answer.append(i)
             while n % i == 0:
-                n //= i   # 중복 제거
-        i += 1
-
+                n //= i
     if n > 1:
         answer.append(n)
-
     return answer
